2c21f1d6c1f34b7a3f16bcee1980dda45122732b
Colin Domoney <colin@createk-design.com>
2018-04-24 16:11:40 +0200

    Add database Docker images and init script

---
+++ database/init-grafeas-db.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    CREATE USER grafeas WITH LOGIN PASSWORD '9Ptd5EQZFLoufuIby';
+    CREATE DATABASE grafeas;
+    GRANT ALL PRIVILEGES ON DATABASE grafeas TO grafeas;
+EOSQL
+
